6cff496bec8ff14812e09ca3844009a83476a43d
AlessioDPT <alessio.recos@gmail.com>
2021-06-17 18:17:08 +0200

    ADD-milestone2

--- js/script.js
+++ js/script.js
@@ -2,40 +2,41 @@
 //* Tramite il forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
 //* Milestone 2 Dividere i gatti in due contenitori distinti in base al sesso e aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio. Il colore del fiocco deve essere più tenue se il gatto è più giovane, più scuro se il gatto è più vecchio.
 //* Milestone 3 Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio, inserendo solamente nome, colore e opacità del fiocco per ogni gatto.
-//* Fate la prima Milestone da soli e poi attendete in comunicazioni l'orario del collegamento.
 
 
 
-//* Milestone 1 Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
+
+//! Milestone 1
+//* Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
 const gruppoGatti = [
     {
         nome : "Salem",
         eta : 7,
-        colore : "nero",
+        colore : "#0a0a0a",
         sesso : "maschio"
     },
     {
         nome : "Garfield",
         eta : 3,
-        colore : "arancione",
+        colore : "#fc8403",
         sesso : "maschio"
     },
     {
         nome : "Romeo",
         eta : 9,
-        colore : "arancione striato",
+        colore : "#f2bc3f",
         sesso : "maschio"
     },
     {
         nome : "Duchessa",
         eta : 10,
-        colore : "grigio",
+        colore : "#7d7c79",
         sesso : "femmina"
     },
     {
         nome : "Minù",
         eta : 2,
-        colore : "bianco",
+        colore : "#f5f4f2",
         sesso : "femmina"
     },
 ];
@@ -43,8 +44,59 @@ const gruppoGatti = [
 //* Tramite il forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
 
 gruppoGatti.forEach((element) => {
-    document.getElementById("nome_gatti").innerHTML += 
+    document.getElementById("nome_gatti").innerHTML +=
     `
-        C'è un gatto che si chiama: ${element.nome} e ha il colore ${element.colore}. <br>
+        C'è un gatto che si chiama ${element.nome} e ha il colore <i class="fas fa-cat" style="color:${element.colore}"></i>.<br>
     `;
 });
+
+
+
+//! Milestone 2
+//* Dividere i gatti in due contenitori distinti in base al sesso e aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio. Il colore del fiocco deve essere più tenue se il gatto è più giovane, più scuro se il gatto è più vecchio.
+
+
+const rosa = "pink";
+const blue = "blue";
+
+
+const nGruppoGatti = gruppoGatti.map((element) => {
+
+    const nome = element.nome;
+    const eta = element.eta;
+    const colore = element.colore;
+    const sesso = element.sesso;
+    
+
+    //* a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio.
+    let fioccoColore = blue;
+    if (sesso === "femmina"){
+        fioccoColore = rosa;
+    }
+    
+
+    //* Il colore del fiocco deve essere più tenue se il gatto è più giovane, più scuro se il gatto è più vecchio.
+    const opacita = element.eta / 10;
+
+    return {
+        nome,
+        eta,
+        colore,
+        sesso,
+        fiocco : {
+            colore : fioccoColore,
+            opacita : opacita
+
+        }
+
+    }
+});
+
+// Ristampa dei gatti con il fiocco.
+nGruppoGatti.forEach((element) => {
+    document.getElementById("nome_gatti_fiocco").innerHTML +=
+    `
+        C'è un gatto che si chiama ${element.nome} e ha il colore <i class="fas fa-cat" style="color:${element.colore}"></i> <i class="fas fa-award" style="color:${element.fiocco.colore}; opacity:${element.fiocco.opacita}"></i>.<br>
+    `;
+});
+
